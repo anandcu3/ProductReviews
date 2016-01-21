cfe78574bb1880f7b15dfa8e198ec061b2a7da10
anandcu3 <anandcu3@gmail.com>
2016-01-21 17:24:03 +0530

    FrontEnd Done lo

--- app/src/main/java/anand/productreviews/MainActivity.java
+++ app/src/main/java/anand/productreviews/MainActivity.java
@@ -1,5 +1,8 @@
 package anand.productreviews;
 
+import android.app.Activity;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -9,15 +12,34 @@ import android.view.View;
 import android.app.FragmentManager;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 
 public class MainActivity extends FragmentActivity {
 
+    Context c;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new AddSpinner().execute();
+        c = this;
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -45,5 +67,81 @@ public class MainActivity extends FragmentActivity {
         FragmentManager fm = getFragmentManager();
         Choice my_dialog= new Choice();
         my_dialog.show(fm,"my_dialog");
+    }
+
+    public void Search( View v)
+    {
+
+    }
+
+
+    //AsyncTask to add to spinner
+    private class AddSpinner extends AsyncTask<String,JSONObject,String[]> {
+
+
+        @Override
+        protected String[] doInBackground(String... arg) {
+            // TODO Auto-generated method stub
+            try {
+                HttpClient client = new DefaultHttpClient();
+
+
+                HttpGet post = new HttpGet("http://reviewrating.esy.es/productlist.php");
+
+
+                HttpResponse response = client.execute(post);
+
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"), 8);
+                StringBuilder sb = new StringBuilder();
+
+                String line = "";
+                while ((line = br.readLine()) != null) {
+                    sb.append(line);
+                }
+
+                String products = sb.toString();
+                String[] lp = products.split(" ");
+                return lp;
+                //ArrayList<MyRow> al = new ArrayList<MyRow>();
+
+            /*    try {
+
+                    JSONArray arr = new JSONArray( sb.toString() );
+                    for( int i=0 ;i<arr.length()  ; i++)
+                    {
+                        JSONObject obj = arr.getJSONObject(i);
+
+                        al.add( new MyRow( obj.getString("name") , obj.getString("msg"))   );
+                    }
+                    return al;
+                } catch (JSONException e) {
+                    // TODO Auto-generated catch block
+                    //Toast.makeText(c,e.toString(),Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+*/
+
+            } catch (ClientProtocolException e) {
+                // TODO Auto-generated catch block
+                //publishProgress();
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                //publishProgress();
+                e.printStackTrace();
+            }
+            return new String[]{"emoty"};
+        }
+
+
+
+
+        protected void onPostExecute(String[] lp) {
+            Spinner s = (Spinner) findViewById(R.id.products_spinner);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(c,android.R.layout.simple_spinner_item,lp);
+            s.setAdapter(adapter);
+            //Toast.makeText(c,"Spinner ge add aaytu", Toast.LENGTH_SHORT).show();
+        }
     }
 }
